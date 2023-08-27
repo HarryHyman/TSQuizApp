0c9f40e09930c5f395f1073615f8247a9b8d4a9a
Harry <harryhyman7@gmail.com>
2023-08-27 17:20:52 +0100

    Implement login and bcrypt hashing

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -3,11 +3,12 @@ import { Router } from "express";
 import { UserController } from "../controllers/user.controller";
 import { RegisterUser, registerUserValidator } from "../validators/registerUser.validator";
 import validator from "../middlewares/validator";
+import { LoginUser, loginUserValidator } from "../validators/loginUser.validator";
 
 const router = Router();
 
 router.post("/register", validator<RegisterUser>(registerUserValidator), UserController.registerUser);
 
-router.post("/login", UserController.login);
+router.post("/login", validator<LoginUser>(loginUserValidator), UserController.login);
 
 export default router;
